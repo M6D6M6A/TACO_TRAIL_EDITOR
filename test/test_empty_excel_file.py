a33e340a45147a40f127d84d544ec09920d75107
Philipp Reuter <reuter.philipp@ymail.com>
2021-10-11 20:30:26 +0200

    .

---
+++ test/test_empty_excel_file.py
@@ -0,0 +1,12 @@
+import sys
+import unittest
+import taco_trail_editor
+sys.path.insert(0, "../taco_trail_editor")
+DEBUG = True
+
+
+class test_taco_trail_editor(unittest.TestCase):
+    def test_1(self):
+        """ Test 1 """
+        
+        self.assertEqual(1, 1)
